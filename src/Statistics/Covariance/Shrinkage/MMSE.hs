03da4af688f67ca4b9ddcc8701a20daf1dfe08fe
Thayne Batty <tsbattman@gmail.com>
2019-01-25 11:26:01 +0800

    function to shrink cov matrix itself given number of samples

--- src/Statistics/Covariance/Shrinkage/MMSE.hs
+++ src/Statistics/Covariance/Shrinkage/MMSE.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ViewPatterns #-}
 
 module Statistics.Covariance.Shrinkage.MMSE (
     mmseCov
@@ -5,7 +6,9 @@ module Statistics.Covariance.Shrinkage.MMSE (
   , sampleCov
   , naiveCov
   , lwShrinkageCoef
+  , rblwShrinkCovCoef
   , rblwShrinkageCoef
+  , oracleShrinkCovCoef
   , oracleShrinkageCoef
   ) where
 
@@ -27,11 +30,14 @@ shrinkCov sample coef target = scale (1 - coef) sample `add` scale coef target
 sampleCov :: Matrix Double -> Herm Double
 sampleCov = snd . meanCov
 
-naiveCov :: Matrix Double -> Herm Double
-naiveCov x = trustSym . scale s' $ ident p
+avgVarCov :: Herm Double -> Herm Double
+avgVarCov (unSym -> s) = trustSym $ scale s' (ident p)
   where
-    p = cols x
-    s' = mean . takeDiag . unSym $ sampleCov x
+    p = cols s
+    s' = mean $ takeDiag s
+
+naiveCov :: Matrix Double -> Herm Double
+naiveCov = avgVarCov . sampleCov
 
 lwShrinkageCoef :: Matrix Double -> Double
 lwShrinkageCoef x = numer / denom
@@ -45,16 +51,18 @@ lwShrinkageCoef x = numer / denom
     numer = sumVector kbn (VU.generate n frob) / fromIntegral (sq n)
     denom = trs2 - tr2s / fromIntegral p
 
-rblwShrinkageCoef :: Matrix Double -> Double
-rblwShrinkageCoef x = numer / denom
+rblwShrinkCovCoef :: Int -> Herm Double -> Double
+rblwShrinkCovCoef (fromIntegral -> n) (unSym -> s) = numer / denom
   where
-    n = fromIntegral $ rows x
-    p = cols x
-    s = unSym $ sampleCov x
+    p = cols s
     trs2 = trace $ s <> s
     tr2s = sq $ trace s
     numer = (n - 2) / n * trs2 + tr2s
     denom = (n + 2) * (trs2 - tr2s / fromIntegral p)
+
+rblwShrinkageCoef :: Matrix Double -> Double
+rblwShrinkageCoef x = rblwShrinkCovCoef n (sampleCov x)
+  where n = rows x
 
 data OASIter = OASIter Double (Herm Double)
 
@@ -79,13 +87,15 @@ oracleIter tol oas = go 1 . oracleStep oas
       | abs (p - p') < tol = p'
       | otherwise = go p' $ oracleStep oas sj'
 
-oracleShrinkageCoef :: Matrix Double -> Double
-oracleShrinkageCoef x = oracleIter 1e-6 (OAS n p s f) s
+oracleShrinkCovCoef :: Int -> Herm Double -> Double
+oracleShrinkCovCoef n s = oracleIter 1e-6 (OAS n p s f) s
   where
-    n = rows x
-    p = cols x
-    s = sampleCov x
-    f = trustSym $ scale (mean (takeDiag (unSym s))) (ident p)
+    p = cols $ unSym s
+    f = avgVarCov s
+
+oracleShrinkageCoef :: Matrix Double -> Double
+oracleShrinkageCoef x = oracleShrinkCovCoef n (sampleCov x)
+  where n = rows x
 
 sq :: Num a => a -> a
 sq a = a * a
